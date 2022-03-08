8c66396f5590bdf373f52ecfdd426d1055d1f9ef
Elizabeth <elizabeth.por@gmail.com>
2022-03-08 11:56:44 -0600

    made a template for the Artist category

--- src/Components/CatArtistsAuthorsEntertainers.js
+++ src/Components/CatArtistsAuthorsEntertainers.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import AuthorCard from './AuthorCard';
 
-function CatArtistsAuthorsEntertainers() {
+// we should probably do the fetching and setting state in NavBar so it's 
+//available to all categories, then pass down quotes as prop to here
+
+function CatArtistsAuthorsEntertainers({ authors }) {
+
+    const [quotes, setQuotes] = useState([])
+
+    const {id} = authors;
+
+    function handleClick() {
+        fetch(`https://api.fisenko.net/v1/authors/en/${id}/quotes?query=&limit=1&offset=0`)
+        .then(response => response.json())
+        .then(quotesData => setQuotes(quotesData))
+    }
+
     return (
-        <div>CatArtistsAuthorsEntertainers</div>
+        <div>
+            <h1>Artists, Authors, and Entertainers</h1>
+            <h2 onClick={handleClick}>Lady Gaga</h2>
+            <h2 onClick={handleClick}>Another artist</h2>
+            <h2 onClick={handleClick}>Another one</h2>
+            <h2 onClick={handleClick}>One more</h2>
+            <h2 onClick={handleClick}>Just one more</h2>
+            <AuthorCard quotes={quotes}/>
+        </div>
     )
 }
 
