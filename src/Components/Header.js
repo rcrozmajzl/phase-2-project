cbd9fbf91e554a2db9a1fc3e842100ef1962211f
Elizabeth <elizabeth.por@gmail.com>
2022-03-08 10:33:36 -0600

    installed react-router-dom and filled in Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { logo } from '../logo.svg'; // need to fix this
+import Search from './Search';
 
+// by clicking on the logo you go back to Home, aka Link exact to='/'
 function Header() {
     return (
-        <div>Header</div>
+        <div className='header'>
+            <Link exact to='/'>
+                <img src={logo} alt='logo' />
+            </Link>
+            <h1>Get Inspired</h1>
+            <Search />
+        </div>
     )
 }
 
-export default Header
+export default Header;
