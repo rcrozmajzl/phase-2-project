9cd6c4a01f2df72f9b1800fecdf2ee2af2e29de4
Elizabeth <elizabeth.por@gmail.com>
2022-03-08 11:31:03 -0600

    built search, updated header, built home

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { logo } from '../logo.svg'; // need to fix this
+import { logo } from '../logo.svg'; //need to fix this
 import Search from './Search';
 
 // by clicking on the logo you go back to Home, aka Link exact to='/'
-function Header() {
+
+function Header({ onSearchInput, searchText }) {
+
     return (
         <div className='header'>
             <Link exact to='/'>
                 <img src={logo} alt='logo' />
             </Link>
             <h1>Get Inspired</h1>
-            <Search />
+            <Search 
+                onSearchInput={onSearchInput}
+                searchText={searchText}
+            />
         </div>
     )
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Header from './Header';
+import NavBar from './NavBar';
+import authors from '../../authors.json';
 
 function Home() {
+
+    const [searchText, setSearchText] = useState('');
+
+    function onSearchInput(searchedValue) {
+        setSearchText(searchedValue)
+    }
+
+    const authorToDisplay = authors.filter((author) => author.name.toLowerCase().includes(searchText.toLowerCase()))
+
+    // sending authors and authorToDisplay down to NavBar to pass down to Categories
     return (
-        <div>Home</div>
+        <div>
+            <Header onSearchInput={onSearchInput}/>
+            <NavBar authorToDisplay={authorToDisplay} authors={authors}/> 
+        </div>
     )
 }
 
-export default Home
+export default Home;
