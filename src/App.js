eb8a0e2672d18aec20abdc0b3db8b7d69fdf8dcf
Elizabeth <elizabeth.por@gmail.com>
2022-03-08 20:17:16 -0600

    created client router, changed hierarchy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home';
+import Header from './Components/Header';
+import NavBar from './Components/NavBar';
+import Category from './Components/Category';
+import AuthorCard from './Components/AuthorCard';
+import Comments from './Components/Comments';
+
 
 function App() {
+
+      // const [searchText, setSearchText] = useState('');
+      const [quotes, setQuotes] = useState([]);
+    
+      const DATABASE = 'http://localhost:8001/quotes';
+  
+      useEffect(() => {
+          fetch(DATABASE)
+          .then(res => res.json())
+          .then(quotesData => console.log(quotesData))
+      }, [])
+  
+      // function onSearchInput(searchedValue) {
+      //     setSearchText(searchedValue)
+      // }
+  
+      // const authorToDisplay = authors.filter((author) => author.name.toLowerCase().includes(searchText.toLowerCase()))
+
   return (
     <div className="App">
-      <Home />
+      <Header />
+      <NavBar quotes={quotes}/>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/:category">
+          <Category />
+        </Route>
+        <Route exact path="/:author">
+          <AuthorCard />
+        </Route>
+        <Route exact path="/:author/comments">
+          <Comments />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,30 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import Header from './Header';
-import NavBar from './NavBar';
+import React from 'react';
 
 function Home() {
 
-    // const [searchText, setSearchText] = useState('');
-    const [quotes, setQuotes] = useState([]);
-    
-    const DATABASE = 'http://localhost:8001/quotes';
-
-    useEffect(() => {
-        fetch(DATABASE)
-        .then(res => res.json())
-        .then(quotesData => console.log(quotesData))
-    }, [])
-
-    // function onSearchInput(searchedValue) {
-    //     setSearchText(searchedValue)
-    // }
-
-    // const authorToDisplay = authors.filter((author) => author.name.toLowerCase().includes(searchText.toLowerCase()))
-
     return (
         <div>
-            <Header />
-            <NavBar quotes={quotes}/> 
+
         </div>
     )
 }
