a9eb1cae320e049ec95c39f7237eac08266ef0c3
unlimited1974@aol.com <unlimited1974@aol.com>
2022-03-09 14:04:15 -0600

    Updated Form 2pm

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -28,7 +28,7 @@ function Comments() {
                 method: "POST",
                 headers: {"Content-Type": "application/json",
             },
-            body: JSON.stringify()
+            body: JSON.stringify(commentData)
             })
             .then(res => res.json())
             .then(comment => setCommentArray([...commentArray, comment]))
