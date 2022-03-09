cf131f048f7b381ad5b8297083697a0db23bb1e5
Elizabeth <elizabeth.por@gmail.com>
2022-03-09 15:16:35 -0600

    changed categories.js and category.js

--- src/categories.js
+++ src/categories.js
@@ -1,33 +1,104 @@
 export const categories = [
+    // {
+    //     "id": 1,
+    //     "type": "Athletes",
+    //     "authors": ["Serena Williams", "Rafal Nadal", "Bode Miller", "Michael Schumacher", "Cristiano Ronaldo" ],
+    //     "authorIds": [1, 2, 3, 4, 5]
+    // },
     {
         "id": 1,
         "type": "Athletes",
-        "authors": ["Serena Williams", "Rafal Nadal", "Bode Miller", "Michael Schumacher", "Cristiano Ronaldo" ],
-        "authorIds": [1, 2, 3, 4, 5]
+        "authors": [
+            {"authorName": "Serena Williams",
+            "authorId": 1},
+            {"authorName": "Rafal Nadal",
+            "authorId": 2},
+            {"authorName": "Bode Miller",
+            "authorId": 3},
+            {"authorName": "Michael Schumacher",
+            "authorId": 4},
+            {"authorName": "Cristiano Ronaldo",
+            "authorId": 5}
+        ]
     },
     {
         "id": 2,
         "type": "Philosophers and Religious Figures",
-        "authors": ["Aristotle", "David Hume", "John Stuart Mill", "Jean-Paul Sartre", "Socrates"]
+        "authors": [
+            {"authorName": "Aristotle",
+            "authorId": 6},
+            {"authorName": "David Hume",
+            "authorId": 7},
+            {"authorName": "John Stuart Mill",
+            "authorId": 8},
+            {"authorName": "Jean-Paul Sartre",
+            "authorId": 9},
+            {"authorName": "Socrates",
+            "authorId": 10}
+        ]
     },
     {
         "id": 3,
         "type": "Tech Icons and Inventors",
-        "authors": ["Steve Jobs", "Elon Musk", "Thomas A Edison", "Anne Wojcicki", "Grace Hopper"]
+        "authors": [
+            {"authorName": "Steve Jobs",
+            "authorId": 11},
+            {"authorName": "Elon Musk",
+            "authorId": 12},
+            {"authorName": "Thomas A Edison",
+            "authorId": 13},
+            {"authorName": "Anne Wojcicki",
+            "authorId": 14},
+            {"authorName": "Grace Hopper",
+            "authorId": 15}
+        ]
     },
     {
         "id": 4,
         "type": "Political Figures",
-        "authors": ["Abraham Lincoln", "Eleanor Roosevelt", "Condoleeza Rice", "Nelson Mandela", "Golda Meir"]
+        "authors": [
+            {"authorName": "Abraham Lincoln",
+            "authorId": 16},
+            {"authorName": "Eleanor Roosevelt",
+            "authorId": 17},
+            {"authorName": "Condoleeza Rice",
+            "authorId": 18},
+            {"authorName": "Nelson Mandela",
+            "authorId": 19},
+            {"authorName": "Golda Meir",
+            "authorId": 20}
+        ]
     },
     {
         "id": 5,
         "type": "Artists, Authors, and Entertainers",
-        "authors": ["John Lennon", "Beyonce", "Frank Zappa", "Carole King", "Stevie Ray Vaughan"]
+        "authors": [
+            {"authorName": "John Lennon",
+            "authorId": 21},
+            {"authorName": "Beyonce",
+            "authorId": 22},
+            {"authorName": "Frank Zappa",
+            "authorId": 23},
+            {"authorName": "Carole King",
+            "authorId": 24},
+            {"authorName": "Stevie Ray Vaughan",
+            "authorId": 25}
+        ]
     },
     {
         "id": 6,
         "type": "Financial Gurus",
-        "authors": ["Milton Friedman", "Dave Ramsey", "Donald Trump", "Robert T. Kiyosaki", "Warren Buffet"]
+        "authors": [
+            {"authorName": "Milton Friedman",
+            "authorId": 26},
+            {"authorName": "Dave Ramsey",
+            "authorId": 27},
+            {"authorName": "Donald Trump",
+            "authorId": 28},
+            {"authorName": "Robert T. Kiyosaki",
+            "authorId": 29},
+            {"authorName": "Warren Buffet",
+            "authorId": 30}
+        ]
     }
 ]
