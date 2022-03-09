7fc1e534997fb4433742cc8281ab465db9005df7
unlimited1974@aol.com <unlimited1974@aol.com>
2022-03-08 19:08:49 -0600

    CommentForm Buildout continued

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -1,17 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CommentForm from './CommentForm';
 import CommentDisplay from './CommentDisplay';
+
+const API = "http://localhost:8001/comments"
 
 function Comments() {
 
     const [commentArray, setCommentArray] = useState([]);
+    const [commentInput, setCommentInput] = useState("");
 
-    const comment = commentArray.map(comments => <CommentDisplay key={comments.id} {...comments} /> )
+    const comment = commentArray.map(comments => <CommentDisplay key={comments.id} {...comments} />)
+
+    useEffect(() => {
+        fetch(API)
+            .then((res) => res.json())
+            .then((data) => setCommentArray(data))
+    }, []);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const commentData = {content: commentInput}
+        const newComment = [...commentInput, commentData]
+
+        setCommentInput("")
+            fetch(API, {
+                method: "POST",
+                headers: {"Content-Type": "application/json",
+            },
+            body: JSON.stringify()
+            })
+            .then(res => res.json())
+            .then(comment => setCommentArray([...commentArray, comment]))
+        }
 
     return (
         <div>
             {comment}
-            <CommentForm />
+            <CommentForm 
+                handleSubmit={handleSubmit}
+                commentInput={commentInput}
+            />
         </div>
     )
 }
