939987247e66f7a2bdb5d1f0d6f267060d756ab0
Rebecca Rozmajzl <rebecca.rozmajzl@gmail.com>
2022-03-10 11:07:13 -0500

    more css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import title from '../assets/title.png'
+// import title from '../assets/title.png'
 // import Search from './Search';
 
 function Header() {
 
     return (
         <div className='header'>
-            <img className="title" src={title} alt='title' />
+            <h1>Get Inspired</h1>
+            {/* <img className="title" src={title} alt='title' /> */}
             {/* <Search 
                 onSearchInput={onSearchInput}
                 searchText={searchText}
