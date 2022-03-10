c3d4a15f4b5af2ae2926577b2f7c4c30a58f8ac6
Rebecca Rozmajzl <rebecca.rozmajzl@gmail.com>
2022-03-10 11:52:01 -0500

    finished home and navbar css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,9 +5,17 @@ function Home() {
 
     return (
         <div className="home">
-            <h2>Welcome to Get Inspired!</h2>
-            <p>Here you can explore our collection of inspirational quotes to help you find the peace, encouragement, and motivation you need!</p>
-            <h3>Browse any of our pre-selected authors by category along the left side of your screen!</h3>
+            <h2>Welcome!</h2>
+            <br/>
+            <h4>Here you can explore our collection of inspirational quotes to help you find the peace, encouragement, and motivation you need</h4>
+            <br/>
+            <br/>
+            <hr/>
+            <br/>
+            <br/>
+            <h4>Browse any of our pre-selected authors by category along the left side of your screen</h4>
+            <br/>
+            <br/>
             <img className="arrow" src={arrow} alt="arrow"/>
         </div>
     )
