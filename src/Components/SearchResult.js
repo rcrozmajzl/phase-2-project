d8d04c63d70e860bae7dd6dcc160a963f77061a3
Elizabeth <elizabeth.por@gmail.com>
2022-03-10 17:42:14 -0600

    last changes on search

--- src/Components/SearchResult.js
+++ src/Components/SearchResult.js
@@ -12,14 +12,16 @@ function SearchResult() {
     const allAuthors = categories.map((category) => category.authors).flat()
     const matchingAuthors = allAuthors.filter((author) => author.authorName.toLowerCase().includes(authorName.toLowerCase()))
 
-
-
     const authorLinks = matchingAuthors.map((author) => <Link key={author.authorId} to={`/author/${author.authorId}`}> 
     <h2 className="authorLinks">{author.authorName}</h2> </Link>)
     
+    // if (matchingAuthors === undefined) {
+    //     return <h2>Ooops we don't have that Author!</h2>;
+    // }
+
     return (
         <div>
-            <h2>Searched authors</h2>
+            <h2>Searched Authors</h2>
             {authorLinks}
         </div>
     )
